9204d8b0cd9165c7a0e31b289cfaef3e4ea9169c
Halfneck <eurowoa@yahoo.com>
2024-12-17 09:36:58 -0500

    # timecapsule

---
+++ mywork/moves.py
@@ -0,0 +1,11 @@
+weather = "sunny"
+
+if weather == "sunny":
+    print("wear sunglasses!")
+else:
+    print("Take an unbrella!")
+
+how_are_you = input("good")
+print("That's great to hear!")
+if how_are_you == input("sad"):
+    print("i hope you feel better!")
